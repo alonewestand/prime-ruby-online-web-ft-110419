6917e9c4f7ea0a375bc7c89cdc3322d6157bbb0a
Ken Goad <goadke@g.cofc.edu>
2019-11-08 22:43:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(int)
   if int < 0 | num == 0 | num = 1
   return false
   else
-    (2..num-1).to_a.all? do |isit_prime|
-      num % isit_prime != 0
+    (2..int-1).to_a.all? do |isit_prime|
+      int % isit_prime != 0
   end
   end
 end
